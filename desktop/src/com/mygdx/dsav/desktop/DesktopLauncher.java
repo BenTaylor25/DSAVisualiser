f607d36d2aa4157686a3ecaea57b30f5ae574757
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-03 15:55:21 +0100

    config

--- desktop/src/com/mygdx/dsav/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/dsav/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.mygdx.dsav.DSAVisualiser;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "DSAVisualiser";
+		config.useGL30 = true;
+		config.width = 1280;
+		config.height = 720;
+		//config.resizable = false;
 		new LwjglApplication(new DSAVisualiser(), config);
 	}
 }
