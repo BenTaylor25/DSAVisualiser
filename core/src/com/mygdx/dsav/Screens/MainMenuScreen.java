5880ec3d93f56568d1069f056ca3b29db849f7ef
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-09 14:24:39 +0100

    added hover highlighting on main page

--- core/src/com/mygdx/dsav/Screens/MainMenuScreen.java
+++ core/src/com/mygdx/dsav/Screens/MainMenuScreen.java
@@ -24,6 +24,7 @@ public class MainMenuScreen extends FactOption {
     BenHelper.Rect exitButtonBox;
     BenHelper.Rect stackButtonBox;
     BenHelper.Rect myGHButtonBox;
+    BenHelper.Rect fontLinkButtonBox;
 
     public MainMenuScreen() {
         GW = Gdx.graphics.getWidth();
@@ -39,7 +40,8 @@ public class MainMenuScreen extends FactOption {
 
         exitButtonBox = new BenHelper.Rect(GW*0.88f, 0, GW*0.1f, GH*0.09f);
         stackButtonBox = new BenHelper.Rect(GW*0.19f, GH*0.725f, GW*0.25f, GH*0.085f);
-        myGHButtonBox = new BenHelper.Rect(GW *0.001f, GH*0.06f, GW*0.175f, GH*0.08f);
+        myGHButtonBox = new BenHelper.Rect(GW*0.001f, GH*0.06f, GW*0.175f, GH*0.08f);
+        fontLinkButtonBox = new BenHelper.Rect(GW*0.001f, GH*0.001f, GW*0.38f, GH*0.05f);
     }
 
     @Override
@@ -64,23 +66,38 @@ public class MainMenuScreen extends FactOption {
 
         batch.begin();
             ScreenUtils.clear(0.15f, 0.25f, 0.35f, 1f);
+            Color baseCol = new Color(0.69f, 0.447f, 0.098f, 1);
+            Color hoverCol = new Color(1, 1, 1, 1);
+            boolean hoverHere = false;
 
-            font.setColor(0.69f, 0.447f, 0.098f, 1);
+            font.setColor(baseCol);
             font.getData().setScale(2);
             font.draw(batch, "DSAVisualiser", GW*0.2f, GH*0.9f);
 
+            hoverHere = stackButtonBox.checkHover();
+            if (hoverHere) { font.setColor(hoverCol); }
             font.getData().setScale(1.125f);
             font.draw(batch, "Stack", GW*0.2f, GH*0.775f);
+            if (hoverHere) { font.setColor(baseCol); }
 
+            hoverHere = myGHButtonBox.checkHover();
+            if (hoverHere) { font.setColor(hoverCol); }
             font.getData().setScale(1);
             font.draw(batch, "Ben Taylor", GW*0.01f, GH*0.1f);
+            if (hoverHere) { font.setColor(baseCol); }
 
+            hoverHere = fontLinkButtonBox.checkHover();
+            if (hoverHere) { font.setColor(hoverCol); }
             font.getData().setScale(0.5f);
             font.draw(batch, "Thanks to Riciery Leal for the VCR OSD Mono font",
                 GW*0.01f, GH*0.03f);
+            if (hoverHere) { font.setColor(baseCol); }
 
+            hoverHere = exitButtonBox.checkHover();
+            if (hoverHere) { font.setColor(hoverCol); }
             font.getData().setScale(1);
             font.draw(batch, "Exit", GW*0.9f, GH*0.05f);
+            if (hoverHere) { font.setColor(baseCol); }
         batch.end();
 
         if (BenHelper.DEBUG) {
@@ -88,6 +105,7 @@ public class MainMenuScreen extends FactOption {
             exitButtonBox.draw(shape);
             stackButtonBox.draw(shape);
             myGHButtonBox.draw(shape);
+            fontLinkButtonBox.draw(shape);
         }
     }
 
@@ -96,12 +114,19 @@ public class MainMenuScreen extends FactOption {
         if (exitButtonBox.checkClick()) {
             factSelector = "quit";
         }
-        if (stackButtonBox.checkClick()) {
+        else if (stackButtonBox.checkClick()) {
             factSelector = "stack";
         }
-        if (myGHButtonBox.checkClick()) {
+        else if (myGHButtonBox.checkClick()) {
             try {
                 Desktop.getDesktop().browse(new URL("https://github.com/BenTaylor25").toURI());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else if (fontLinkButtonBox.checkClick()) {
+            try {
+                Desktop.getDesktop().browse(new URL("https://www.dafont.com/vcr-osd-mono.font").toURI());
             } catch (Exception e) {
                 e.printStackTrace();
             }
