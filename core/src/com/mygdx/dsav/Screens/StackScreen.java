10b831dbf5393e5bb7479886f0d70b657cb3e5d6
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-14 15:45:20 +0100

    added hint messages to Stack page

--- core/src/com/mygdx/dsav/Screens/StackScreen.java
+++ core/src/com/mygdx/dsav/Screens/StackScreen.java
@@ -25,12 +25,15 @@ public class StackScreen extends FactOption {
     boolean inputTypingEnabled;
     String inputTextString;
     String outputTextString;
+    String hintTextString;
+    BenHelper.Rect titleButtonBox;
     BenHelper.Rect backButtonBox;
     BenHelper.Rect inputTextButtonBox;
     BenHelper.Rect pushButtonBox;
     BenHelper.Rect outputTextButtonBox;
     BenHelper.Rect peekButtonBox;
     BenHelper.Rect popButtonBox;
+    BenHelper.Rect hintButtonBox;
 
     public StackScreen() {
         GW = Gdx.graphics.getWidth();
@@ -49,12 +52,15 @@ public class StackScreen extends FactOption {
         inputTypingEnabled = false;
         inputTextString = new String("");
         outputTextString = new String("");
+        hintTextString = new String("");
+        titleButtonBox = new BenHelper.Rect(GW*0.4f, GH*0.85f, GW*0.2f, GH*0.15f);
         backButtonBox =  new BenHelper.Rect(0, 0, GW*0.1f, GH*0.1f);
         inputTextButtonBox = new BenHelper.Rect(GW*0.025f, GH*0.525f, GW*0.3f, GH*0.1f);
         pushButtonBox = new BenHelper.Rect(GW*0.105f, GH*0.3f, GW*0.14f, GH*0.14f);
         outputTextButtonBox = new BenHelper.Rect(GW*0.675f, GH*0.525f, GW*0.3f, GH*0.1f);
         peekButtonBox = new BenHelper.Rect(GW*0.675f, GH*0.3f, GW*0.14f, GH*0.14f);
         popButtonBox = new BenHelper.Rect(GW*0.835f, GH*0.3f, GW*0.14f, GH*0.14f);
+        hintButtonBox =  new BenHelper.Rect(GW*0.15f, 0, GW*0.7f, GH*0.1f);
     }
 
     @Override
@@ -73,6 +79,26 @@ public class StackScreen extends FactOption {
 
     @Override
     public String updateBefore(String factSelector) {
+        hintTextString = "";
+        if (titleButtonBox.checkHover()) {
+            hintTextString = "A Stack is a collection of values where \nyou only have access to the top item.";
+        }
+        else if (inputTextButtonBox.checkHover()) {
+            hintTextString = "Type a value to push to the Stack.";
+        }
+        else if (pushButtonBox.checkHover()) {
+            hintTextString = "You can 'Push' values to the top of a \nStack.";
+        }
+        else if (outputTextButtonBox.checkHover()) {
+            hintTextString = "This is the where previous value obtained \nfrom the Stack is displayed.";
+        }
+        else if (peekButtonBox.checkHover()) {
+            hintTextString = "You can 'Peek' at the top value without \nremoving it from the Stack.";
+        }
+        else if (popButtonBox.checkHover()) {
+            hintTextString = "You can 'Pop' the top value from the \nStack.";
+        }
+
         if (inputTypingEnabled) {
             inputTextString = BenHelper.typing(inputTextString, 20);
         }
@@ -89,7 +115,7 @@ public class StackScreen extends FactOption {
             ScreenUtils.clear(BenHelper.BACKGROUND_COLOUR);
             boolean hoverHere;
             font.setColor(BenHelper.DEFAULT_TEXT_COLOUR);
-            
+
             hoverHere = backButtonBox.checkHover();
             if (hoverHere) { font.setColor(BenHelper.HOVER_TEXT_COLOUR);}
             font.getData().setScale(1.25f);
@@ -126,6 +152,9 @@ public class StackScreen extends FactOption {
         peekButtonBox.draw(shape);
         popButtonBox.draw(shape);
 
+        BenHelper.textDrawCentre(batch, font, "Stack", titleButtonBox, 1.5f, 
+            BenHelper.DEFAULT_TEXT_COLOUR);
+
         if (inputTextString.equals("")) {
             BenHelper.textDrawCentre(batch, font, "[click to type]", inputTextButtonBox, 1f,
                 BenHelper.DEFAULT_TEXT_COLOUR);
@@ -139,9 +168,11 @@ public class StackScreen extends FactOption {
         BenHelper.textDrawCentre(batch, font, "Push", pushButtonBox, 1.25f);
         BenHelper.textDrawCentre(batch, font, "Peek", peekButtonBox, 1.25f);
         BenHelper.textDrawCentre(batch, font, "Pop", popButtonBox, 1.25f);
-
+        BenHelper.textDrawCentre(batch, font, hintTextString, hintButtonBox, 0.75f,
+            Color.GRAY);
 
         if (BenHelper.DEBUG) {
+            titleButtonBox.draw(shape, Color.RED);
             backButtonBox.draw(shape, Color.RED);
         }
     }
