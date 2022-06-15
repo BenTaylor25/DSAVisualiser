fcb9c77d556cad9cbee74c79f3f6616f701ede73
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-15 14:17:23 +0100

    create Array screen

---
+++ core/src/com/mygdx/dsav/Screens/ArrayScreen.java
@@ -0,0 +1,34 @@
+package com.mygdx.dsav.Screens;
+
+import com.mygdx.dsav.FactOption;
+
+public class ArrayScreen extends FactOption {
+    
+
+    @Override
+    public void create() {
+        
+    }
+
+    @Override
+    public void reset() {
+        
+    }
+
+    @Override
+    public String updateBefore(String factSelector) {
+        
+        return factSelector;
+    }
+
+    @Override
+    public void draw() {
+        
+    }
+
+    @Override
+    public String updateAfter(String factSelector) {
+        
+        return factSelector;
+    }
+}
