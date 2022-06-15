ed69a9bbcfba40e46ad9cf8847e89acce419071c
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-15 12:11:22 +0100

    added back button to Queue page

--- core/src/com/mygdx/dsav/Screens/QueueScreen.java
+++ core/src/com/mygdx/dsav/Screens/QueueScreen.java
@@ -4,13 +4,14 @@ import com.mygdx.dsav.BenHelper;
 import com.mygdx.dsav.FactOption;
 
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.Color;
 
 public class QueueScreen extends FactOption {
-    
+    BenHelper.Rect backButtonBox;
 
     @Override
     public void create() {
-        
+        backButtonBox = new BenHelper.Rect(0, 0, GW*0.1f, GH*0.1f);
     }
 
     @Override
@@ -30,11 +31,21 @@ public class QueueScreen extends FactOption {
             ScreenUtils.clear(BenHelper.BACKGROUND_COLOUR);
             font.setColor(BenHelper.DEFAULT_TEXT_COLOUR);
         batch.end();
+
+        BenHelper.textDrawCentre(batch, font, "Back", backButtonBox, 1.25f);
+
+        if (BenHelper.DEBUG) {
+            backButtonBox.draw(shape, Color.RED);
+        }
     }
 
     @Override
     public String updateAfter(String factSelector) {
-        
+        if (backButtonBox.checkClick()) {
+            factSelector = "menu";
+        }
+
+
         return factSelector;
     }
 }
