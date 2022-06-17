72676f6f26a8950172f3533481a474cdc5281d3f
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-17 12:58:53 +0100

    made array text interactive

--- core/src/com/mygdx/dsav/Screens/ArrayScreen.java
+++ core/src/com/mygdx/dsav/Screens/ArrayScreen.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.graphics.Color;
 public class ArrayScreen extends FactOption {
     final int ARRAYSIZE = 6;
     String[] arrayTextString;
-    int typingSelection;
+    int typingSelector;
     String hintTextString;
     BenHelper.Rect titleButtonBox;
     BenHelper.Rect backButtonBox;
@@ -21,7 +21,7 @@ public class ArrayScreen extends FactOption {
         titleButtonBox = new BenHelper.Rect(GW*0.4f, GH*0.85f, GW*0.2f, GH*0.15f);
         backButtonBox = new BenHelper.Rect(0, 0, GW*0.1f, GH*0.1f);
 
-        typingSelection = -1;
+        typingSelector = -1;
         arrayTextString = new String[ARRAYSIZE];
         arrayButtonBoxes = new BenHelper.Rect[ARRAYSIZE];
         for (int i = 0; i < ARRAYSIZE; i++) {
@@ -38,12 +38,22 @@ public class ArrayScreen extends FactOption {
         for (int i = 0; i < ARRAYSIZE; i++) {
             arrayTextString[i] = indexWords[i];
         }
-        typingSelection = -1;
+        typingSelector = -1;
     }
 
     @Override
     public String updateBefore(String factSelector) {
         
+        if (BenHelper.screenClicked()) {
+            typingSelector = -1;
+            for (int i = 0; i < ARRAYSIZE; i++) {
+                if (arrayButtonBoxes[i].checkClick()) {
+                    typingSelector = i;
+                    break;
+                }
+            }
+        }
+
         return factSelector;
     }
 
@@ -61,16 +71,21 @@ public class ArrayScreen extends FactOption {
         }
 
         // draw text 
-        BenHelper.textDrawCentre(batch, font, "Array", titleButtonBox, 1.5f, 
-            BenHelper.DEFAULT_TEXT_COLOUR);
+        BenHelper.textDrawCentre(batch, font, "Array", titleButtonBox, 
+            1.5f, BenHelper.DEFAULT_TEXT_COLOUR
+        );
         for (int i = 0; i < ARRAYSIZE; i++) {
-            BenHelper.textDrawCentre(batch, font, arrayTextString[i], 
-                arrayButtonBoxes[i], 1.25f);
-                
-            BenHelper.textDrawCentre(batch, font, ""+i, 
+            BenHelper.textDrawCentre(
+                batch, font, arrayTextString[i], 
+                arrayButtonBoxes[i], 1f
+            );
+
+            BenHelper.textDrawCentre(
+                batch, font, ""+i, 
                 arrayButtonBoxes[i].x + (arrayButtonBoxes[i].w * 0.5f), 
                 arrayButtonBoxes[i].y - (arrayButtonBoxes[i].h * 0.5f), 
-                1.5f, BenHelper.DEFAULT_TEXT_COLOUR);
+                1.5f, BenHelper.DEFAULT_TEXT_COLOUR
+            );
         }
         BenHelper.textDrawCentre(batch, font, "Back", backButtonBox, 1.25f);
 
@@ -83,9 +98,16 @@ public class ArrayScreen extends FactOption {
 
     @Override
     public String updateAfter(String factSelector) {
-        
+
         if (backButtonBox.checkClick()) {
             factSelector = "menu";
+        }
+
+        if (typingSelector != -1) {
+            arrayTextString[typingSelector] = BenHelper.typing(
+                arrayTextString[typingSelector],
+                10
+            );
         }
 
         return factSelector;
