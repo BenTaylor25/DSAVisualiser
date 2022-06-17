4b5d7f6c4fc9cfef3555703fd145aca1f33549db
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-17 14:59:39 +0100

    capitalised 'Array' in help text

--- core/src/com/mygdx/dsav/Screens/ArrayScreen.java
+++ core/src/com/mygdx/dsav/Screens/ArrayScreen.java
@@ -21,7 +21,7 @@ public class ArrayScreen extends FactOption {
         titleButtonBox = new BenHelper.Rect(GW*0.4f, GH*0.85f, GW*0.2f, GH*0.15f);
         backButtonBox = new BenHelper.Rect(0, 0, GW*0.1f, GH*0.1f);
         hintButtonBox = new BenHelper.Rect(GW*0.15f, 0, GW*0.7f, GH*0.1f);
-        hintTextString = "test";
+        hintTextString = "";
 
         typingSelector = -1;
         arrayTextString = new String[ARRAYSIZE];
@@ -61,7 +61,7 @@ public class ArrayScreen extends FactOption {
                 arrayButtonBoxes[i].checkHover();
         }
         if (hoverOnAnyIndex) {
-            hintTextString = "You can access and modify any item in \nthe array individually.";
+            hintTextString = "You can access and modify any item in \nthe Array individually.";
         }
         else if (titleButtonBox.checkHover()) {
             hintTextString = "An Array is a collection of items that \nare stored in adjacent memory spaces.";
