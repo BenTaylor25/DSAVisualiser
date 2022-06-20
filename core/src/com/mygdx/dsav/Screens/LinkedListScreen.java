c8de1896090191fe74dd8dd21d741c26624638fc
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-20 12:39:29 +0100

    added basic buttons

--- core/src/com/mygdx/dsav/Screens/LinkedListScreen.java
+++ core/src/com/mygdx/dsav/Screens/LinkedListScreen.java
@@ -2,15 +2,27 @@ package com.mygdx.dsav.Screens;
 
 import com.mygdx.dsav.BenHelper;
 import com.mygdx.dsav.FactOption;
-
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class LinkedListScreen extends FactOption {
-    
+    final int LISTSIZE = 4;
+    boolean includeInserted;
+    String[] listTextString;
+    String insertedTextString;
+    int typingSelector;
+    String hintTextString;
+    BenHelper.Rect titleButtonBox;
+    BenHelper.Rect backButtonBox;
+    BenHelper.Rect hintButtonBox;
+
 
     @Override
     public void create() {
-        
+        hintTextString = "";
+        titleButtonBox = new BenHelper.Rect(GW*0.4f, GH*0.85f, GW*0.2f, GH*0.15f);
+        backButtonBox = new BenHelper.Rect(0, 0, GW*0.1f, GH*0.1f);
+        hintButtonBox = new BenHelper.Rect(GW*0.15f, 0, GW*0.7f, GH*0.1f);
     }
 
     @Override
@@ -32,11 +44,29 @@ public class LinkedListScreen extends FactOption {
             font.setColor(BenHelper.DEFAULT_TEXT_COLOUR);
         batch.end();
         
+        // draw button outlines
+
+
+        // draw text
+        BenHelper.textDrawCentre(batch, font, "Linked List", titleButtonBox, 
+            1.5f, BenHelper.DEFAULT_TEXT_COLOUR
+        );
+        BenHelper.textDrawCentre(batch, font, "Back", backButtonBox, 1.25f);
+        BenHelper.textDrawCentre(batch, font, hintTextString, hintButtonBox, 
+            0.75f, Color.GRAY
+        );
+
+        // debug: draw hitboxes
+
     }
 
     @Override
     public String updateAfter(String factSelector) {
         
+        if (backButtonBox.checkClick()) {
+            factSelector = "menu";
+        }
+
         return factSelector;
     }
 }
