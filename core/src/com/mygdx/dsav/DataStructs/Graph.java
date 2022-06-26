a34a215916eda1f846e6c3a8a17508b8d992d571
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-26 13:03:34 +0100

    started on Graph page

---
+++ core/src/com/mygdx/dsav/DataStructs/Graph.java
@@ -0,0 +1,127 @@
+package com.mygdx.dsav.DataStructs;
+
+import java.util.LinkedList;
+
+class GraphNode {
+    protected String value;
+    protected LinkedList<Integer> connections;
+    protected LinkedList<Integer> weights;
+
+    /** Creates a Graph Node (used by the Graph class) */
+    public GraphNode(String val) {
+        value = val;
+        connections = new LinkedList<Integer>();
+        weights = new LinkedList<Integer>();
+    }
+
+    /** String representation of the node. */
+    public String toString(boolean isWeighted, LinkedList<GraphNode> nodes) {
+        String s = "`"+value+"`:{";
+
+        for (int i = 0; i < connections.size(); i++) {
+            int connection = connections.get(i);
+            s += connection+":`"+nodes.get(connection).value+"`";
+
+            if (isWeighted) {
+                s += ":"+weights.get(i);
+            }
+
+            if (!(connection == connections.getLast())) {
+                s += ", ";
+            }
+        }
+
+        s += "}";
+        return s;
+    }
+}
+
+
+public class Graph {
+    private final boolean isWeighted;
+    private final boolean isDirected;
+    private LinkedList<GraphNode> nodes;
+    
+    /** 
+     * Creates a new Graph.
+     * @param weighted Boolean: should the graph be weighted.
+     * @param directed Boolean: should the graph be directed.
+     */
+    public Graph(boolean weighted, boolean directed) {
+        isWeighted = weighted;
+        isDirected = directed;
+        nodes = new LinkedList<>();
+    }
+    /** Creates a new unweighted and undirected Graph. */
+    public Graph() {
+        isWeighted = false;
+        isDirected = false;
+        nodes = new LinkedList<>();
+    }
+
+    /**
+     * Add a node to an existing Graph.
+     * @param val String value that the new node should hold.
+     */
+    public void addNode(String val) {
+        nodes.add(new GraphNode(val));
+    }
+
+    /**
+     * Add a vertex to the graph by referencing two indexes.
+     * @param ind1
+     * @param ind2
+     * @param weight * Parameter is required if Graph is weighted. 
+     */
+    public void addVertex(int ind1, int ind2) {
+        if (!isWeighted) {
+            nodes.get(ind1).connections.add(ind2);
+            if (!isDirected) {
+                nodes.get(ind2).connections.add(ind1);
+            }
+        }
+    }
+    /**
+     * Add a vertex to the graph by referencing two indexes.
+     * @param ind1
+     * @param ind2
+     * @param weight * Parameter is required if Graph is weighted.
+     */
+    public void addVertex(int ind1, int ind2, int weight) {
+        if (isWeighted) {
+            nodes.get(ind1).connections.add(ind2);
+            nodes.get(ind1).weights.add(weight);
+            if (!isDirected) {
+                nodes.get(ind2).connections.add(ind1);
+                nodes.get(ind2).weights.add(weight);
+            }
+        }
+    }
+
+    /** 
+     * Return the value of the node at a given index.
+     * @param ind 
+     * @return String value of the node.
+     */
+    public String valOfNodeAt(int ind) {
+        return nodes.get(ind).value;
+    }
+
+    /** Return a String representation of the Graph with all nodes. */
+    public String toString() {
+        String s = "[";
+
+        for (int i = 0; i < nodes.size(); i++) {
+            GraphNode node = nodes.get(i);
+
+            s += i+":"+node.toString(isWeighted, nodes);
+
+            if (!node.equals(nodes.getLast())) {
+                s += ", ";
+            }
+        }
+
+        s += "]";
+        return s;
+    }
+}
---
+++ core/src/com/mygdx/dsav/Screens/GraphScreen.java
@@ -0,0 +1,74 @@
+package com.mygdx.dsav.Screens;
+
+import com.mygdx.dsav.BenHelper;
+import com.mygdx.dsav.FactOption;
+import com.mygdx.dsav.DataStructs.Graph;
+
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.Color;
+
+public class GraphScreen extends FactOption {
+    Graph graph;
+    String hintTextString;
+    BenHelper.Rect titleButtonBox;
+    BenHelper.Rect backButtonBox;
+    BenHelper.Rect hintButtonBox;
+
+    @Override
+    public void create() {
+        graph = new Graph();
+        hintTextString = "";
+        titleButtonBox = new BenHelper.Rect(GW*0.4f, GH*0.85f, GW*0.2f, GH*0.15f);
+        backButtonBox = new BenHelper.Rect(0, 0, GW*0.1f, GH*0.1f);
+        hintButtonBox = new BenHelper.Rect(GW*0.15f, 0, GW*0.7f, GH*0.1f);
+    }
+
+    @Override
+    public void reset() {
+        graph = new Graph();
+    }
+
+    @Override
+    public String updateBefore(String factSelector) {
+        hintTextString = "";
+        
+        return factSelector;
+    }
+
+    @Override
+    public void draw() {
+        // clear screen
+        batch.begin();
+            ScreenUtils.clear(BenHelper.BACKGROUND_COLOUR);
+            font.setColor(BenHelper.DEFAULT_TEXT_COLOUR);
+        batch.end();
+        
+        // draw static shapes
+
+
+        // draw data items
+
+
+        // draw button outlines
+
+
+        // draw text
+
+
+        // debug: draw hitboxes
+        if (BenHelper.DEBUG) {
+            titleButtonBox.draw(shape, Color.RED);
+            backButtonBox.draw(shape, Color.RED);
+        }
+    }
+
+    @Override
+    public String updateAfter(String factSelector) {
+        if (backButtonBox.checkClick()) {
+            factSelector = "menu";
+        }
+
+
+        return factSelector;
+    }
+}
