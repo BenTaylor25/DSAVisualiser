5ffd17d42bb498c2b8771bf57c2d7eb41b147e8a
Ben Laptop <ben.taylor2573@gmail.com>
2022-06-28 13:14:25 +0100

    controller graph management

--- core/src/com/mygdx/dsav/Screens/GraphScreen.java
+++ core/src/com/mygdx/dsav/Screens/GraphScreen.java
@@ -11,15 +11,18 @@ public class GraphScreen extends FactOption {
     final int GRAPHSIZE = 4;
     Graph graph;
     String hintTextString;
+    int controllerSelector;
     BenHelper.Rect titleButtonBox;
     BenHelper.Rect backButtonBox;
     BenHelper.Rect hintButtonBox;
     BenHelper.Rect[] graphButtonBoxes;
+    BenHelper.Rect[] controllerButtonBoxes;
 
     @Override
     public void create() {
         graph = new Graph();
         hintTextString = "";
+        controllerSelector = -1;
         titleButtonBox = new BenHelper.Rect(GW*0.4f, GH*0.85f, GW*0.2f, GH*0.15f);
         backButtonBox = new BenHelper.Rect(0, 0, GW*0.1f, GH*0.1f);
         hintButtonBox = new BenHelper.Rect(GW*0.15f, 0, GW*0.7f, GH*0.1f);
@@ -30,6 +33,11 @@ public class GraphScreen extends FactOption {
         graphButtonBoxes[2] = new BenHelper.Rect(GW*0.675f, GH*0.425f, GW*0.15f, GH*0.15f);
         graphButtonBoxes[3] = new BenHelper.Rect(GW*0.425f, GH*0.225f, GW*0.15f, GH*0.15f);
 
+        controllerButtonBoxes = new BenHelper.Rect[GRAPHSIZE];
+        controllerButtonBoxes[0] = new BenHelper.Rect(GW*0.9f,  GW*0.11f, GW*0.03f, GW*0.03f);
+        controllerButtonBoxes[1] = new BenHelper.Rect(GW*0.86f, GW*0.07f, GW*0.03f, GW*0.03f);
+        controllerButtonBoxes[2] = new BenHelper.Rect(GW*0.94f, GW*0.07f, GW*0.03f, GW*0.03f);
+        controllerButtonBoxes[3] = new BenHelper.Rect(GW*0.9f,  GW*0.03f, GW*0.03f, GW*0.03f);
     }
 
     @Override
@@ -40,6 +48,25 @@ public class GraphScreen extends FactOption {
     @Override
     public String updateBefore(String factSelector) {
         hintTextString = "";
+
+        // controller
+        if (BenHelper.screenClicked()) {
+            boolean controllerSelected = false;
+            for (int i = 0; i < GRAPHSIZE; i++) {
+                if (controllerButtonBoxes[i].checkHover()) {
+                    controllerSelected = true;
+                    if (controllerSelector != -1 && controllerSelector != i) {
+                        graph.addVertex(controllerSelector, i);
+                        controllerSelector = -1;
+                    } else {
+                        controllerSelector = i;
+                    }
+                }
+            }
+            if (!controllerSelected) {
+                controllerSelector = -1;
+            }
+        }
         
         return factSelector;
     }
@@ -55,6 +82,7 @@ public class GraphScreen extends FactOption {
         // draw static shapes
         for (int i = 0; i < GRAPHSIZE; i++) {
             graphButtonBoxes[i].draw(shape);
+            controllerButtonBoxes[i].draw(shape);
         }
 
         // draw data items
