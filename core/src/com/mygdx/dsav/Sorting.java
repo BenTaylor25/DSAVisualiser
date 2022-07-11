ee37e6ad76d24b9a1faaba46c755fa0186aef5bd
Ben Laptop <ben.taylor2573@gmail.com>
2022-07-11 09:22:45 +0100

    wip sorting algorithm

--- core/src/com/mygdx/dsav/Sorting.java
+++ core/src/com/mygdx/dsav/Sorting.java
@@ -1,12 +1,16 @@
 package com.mygdx.dsav;
 
+import java.util.Arrays;
+
 public class Sorting {
     public static boolean isSorted(String[] arr) {
+
         return false;
     }
 
     public static String[] sort(String[] arr) {
-        return new String[6];
+        String[] srt = Arrays.copyOf(arr, arr.length);
+        return srt;
     }
 
     // generator bubbleStep
