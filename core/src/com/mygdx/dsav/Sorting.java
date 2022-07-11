265419149bcad94b6e02a6337fe1f75813ef5a82
Ben Laptop <ben.taylor2573@gmail.com>
2022-07-11 09:15:52 +0100

    added Sorting class

---
+++ core/src/com/mygdx/dsav/Sorting.java
@@ -0,0 +1,13 @@
+package com.mygdx.dsav;
+
+public class Sorting {
+    public static boolean isSorted(String[] arr) {
+        return false;
+    }
+
+    public static String[] sort(String[] arr) {
+        return new String[6];
+    }
+
+    // generator bubbleStep
+}
