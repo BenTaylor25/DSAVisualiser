a6e9ec18c8a0768b30cbb50f822897b34b8cdb52
Ben Laptop <ben.taylor2573@gmail.com>
2022-07-12 20:12:06 +0100

    added Bubble Sort Generator

---
+++ core/src/com/mygdx/dsav/Generators/GBubbleSort.java
@@ -0,0 +1,41 @@
+package com.mygdx.dsav.Generators;
+
+public class GBubbleSort {
+    int ptrA, ptrB;
+    String[] arr;
+    boolean hasNext;
+
+    public GBubbleSort(String[] values) {
+        arr = values;
+        ptrA = 0;
+        ptrB = 1;
+        hasNext = values.length > 1;
+    }
+
+    public String[] next() {
+        assert hasNext;
+
+        if (shouldSwap(arr[ptrA], arr[ptrB])) {
+            String temp = arr[ptrA];
+            arr[ptrA] = arr[ptrB];
+            arr[ptrB] = temp;
+        }
+
+        ptrB++;
+        if (ptrB == arr.length) {
+            ptrA++;
+            ptrB = ptrA + 1;
+        }
+
+        if (ptrB == arr.length) {
+            hasNext = false;
+        }
+        
+        return arr;
+    }
+    
+    private boolean shouldSwap(String a, String b) {
+        return true;
+    }
+
+}
