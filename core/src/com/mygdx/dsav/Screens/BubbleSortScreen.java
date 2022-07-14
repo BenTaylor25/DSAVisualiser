ee60e4feb7e258d84f86e51fc270685076232100
Ben Laptop <ben.taylor2573@gmail.com>
2022-07-14 15:26:32 +0100

    fixed button sizes

--- core/src/com/mygdx/dsav/Screens/BubbleSortScreen.java
+++ core/src/com/mygdx/dsav/Screens/BubbleSortScreen.java
@@ -22,7 +22,7 @@ public class BubbleSortScreen extends FactOption {
     @Override
     public void create() {
         sorting = false;
-        titleButtonBox = new BenHelper.Rect(GW*0.4f, GH*0.85f, GW*0.2f, GH*0.15f);
+        titleButtonBox = new BenHelper.Rect(GW*0.35f, GH*0.85f, GW*0.3f, GH*0.15f);
         backButtonBox = new BenHelper.Rect(0, 0, GW*0.1f, GH*0.1f);
         hintButtonBox = new BenHelper.Rect(GW*0.15f, 0, GW*0.7f, GH*0.1f);
         sortButtonBox = new BenHelper.Rect(GW*0.425f, GH*0.2f, GW*0.15f, GH*0.15f);
@@ -127,6 +127,7 @@ public class BubbleSortScreen extends FactOption {
         if (BenHelper.DEBUG) {
             titleButtonBox.draw(shape, Color.RED);
             backButtonBox.draw(shape, Color.RED);
+            hintButtonBox.draw(shape, Color.RED);
             sortButtonBox.draw(shape, Color.RED);
         }
     }
