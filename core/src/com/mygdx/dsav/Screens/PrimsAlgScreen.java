dfe846e2a112568bc14f5956e937c6d130e2a9f6
Ben Laptop <ben.taylor2573@gmail.com>
2022-08-12 11:41:09 +0100

    increased title button box size

--- core/src/com/mygdx/dsav/Screens/PrimsAlgScreen.java
+++ core/src/com/mygdx/dsav/Screens/PrimsAlgScreen.java
@@ -31,7 +31,7 @@ public class PrimsAlgScreen extends FactOption {
         controllerSelector = -1;
         typingSelector = -1;
         weightSelector = -1;
-        titleButtonBox = new BenHelper.Rect(GW*0.4f, GH*0.85f, GW*0.2f, GH*0.15f);
+        titleButtonBox = new BenHelper.Rect(GW*0.3f, GH*0.85f, GW*0.4f, GH*0.15f);
         backButtonBox = new BenHelper.Rect(0, 0, GW*0.1f, GH*0.1f);
         hintButtonBox = new BenHelper.Rect(GW*0.15f, 0, GW*0.7f, GH*0.1f);
 
