4edd7c67903c769e8ef27fb5278376af62572f79
Ben Laptop <ben.taylor2573@gmail.com>
2022-08-23 10:34:37 +0100

    GDfsAlg.next()

--- core/src/com/mygdx/dsav/Generators/GDfsAlg.java
+++ core/src/com/mygdx/dsav/Generators/GDfsAlg.java
@@ -1,8 +1,13 @@
 package com.mygdx.dsav.Generators;
 
+import com.mygdx.dsav.DataStructs.Graph;
+
 public class GDfsAlg {
     public String[] nodeValues;
     public boolean hasNext;
+    public Graph visitedTree;
+    private int[][] order;
+    private int orderPtr;
     
     public GDfsAlg(int size) {
         nodeValues = new String[size];
@@ -10,6 +15,23 @@ public class GDfsAlg {
             nodeValues[i] = "";
         }
         hasNext = true;
+
+        order = new int[][]{ 
+            {0, 1}, {0, 2},
+            {1, 3}, {1, 4},
+            {2, 5}, {2, 6}
+        };
+
+        reset();
+    }
+
+    public void reset() {
+        visitedTree = new Graph();
+        for (int i = 0; i < 7; i++) {
+            visitedTree.addNode("");
+        }
+
+        orderPtr = 0;
     }
 
     public boolean allNamed() {
@@ -20,5 +42,15 @@ public class GDfsAlg {
         }
 
         return rv;
+    }
+
+    public void next() {
+        assert hasNext;
+
+        int[] joinNodes = order[orderPtr];
+        visitedTree.addVertex(joinNodes[0], joinNodes[1]);
+        orderPtr++;
+
+        hasNext = orderPtr == order.length;
     }
 }
