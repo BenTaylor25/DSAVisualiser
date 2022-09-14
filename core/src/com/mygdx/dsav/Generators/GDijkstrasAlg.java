70dd10624ca8d63a10c5fbfc532328b216110e45
Ben Laptop <ben.taylor2573@gmail.com>
2022-09-14 10:57:53 +0100

    Dijkstra's Generator reset

--- core/src/com/mygdx/dsav/Generators/GDijkstrasAlg.java
+++ core/src/com/mygdx/dsav/Generators/GDijkstrasAlg.java
@@ -10,10 +10,18 @@ public class GDijkstrasAlg {
 
     public GDijkstrasAlg(int size) {
         _size = size;
+        hasNext = true;
         graph = new Graph(true, false);
         for (int i = 0; i < _size; i++) {
             graph.addNode("");
         }
+    }
+
+    public void reset() {
         hasNext = true;
+        graph = new Graph(true, false);
+        for (int i = 0; i < _size; i++) {
+            graph.addNode("");
+        }
     }
 }
