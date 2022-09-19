b34f3ce148172a1433f5033c2d81d83b774306e3
Ben Taylor <ben.taylor2573@gmail.com>
2022-09-19 14:52:26 +0100

    added node data

--- core/src/com/mygdx/dsav/Generators/GDijkstrasAlg.java
+++ core/src/com/mygdx/dsav/Generators/GDijkstrasAlg.java
@@ -4,9 +4,23 @@ import java.util.ArrayList;
 
 import com.mygdx.dsav.DataStructs.Graph;
 
+class DijkNodeData {
+    public String nodeName;
+    public int minWeight;
+    public String prevNode;
+
+    public DijkNodeData(String name, int weight, String prev) {
+        nodeName = name;
+        minWeight = weight;
+        prevNode = prev;
+    }
+}
+
 public class GDijkstrasAlg {
     public Graph graph;
-    public ArrayList<Integer> visitedNodes;
+    // public ArrayList<Integer> visitedNodes;
+    public Graph visitedGraph;
+    public ArrayList<DijkNodeData> nodeData;
     
     public boolean hasNext;
     private boolean hasStarted;
@@ -17,8 +31,11 @@ public class GDijkstrasAlg {
         hasNext = true;
         hasStarted = false;
         graph = new Graph(true, false);
+        visitedGraph = new Graph();
         for (int i = 0; i < _size; i++) {
             graph.addNode("");
+            visitedGraph.addNode("");
+            nodeData.add(new DijkNodeData("", 99999, "None"));
         }
     }
 
@@ -37,9 +54,10 @@ public class GDijkstrasAlg {
         // if just pressed start
         if (!hasStarted) {
             assert typingSelector != -1;
-            visitedNodes.add(typingSelector);
+            // visitedNodes.add(typingSelector);
             hasStarted = true;
         }
+
 
     }
 }
