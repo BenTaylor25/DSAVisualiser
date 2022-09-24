99479cd6b704ab1eaaebb05ddc127084018d66fc
Ben Taylor <ben.taylor2573@gmail.com>
2022-09-24 10:15:36 +0100

    added window icon

--- desktop/src/com/mygdx/dsav/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/dsav/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.mygdx.dsav.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.dsav.DSAVisualiser;
@@ -11,6 +12,9 @@ public class DesktopLauncher {
 		config.useGL30 = true;
 		config.width = 1280;
 		config.height = 720;
+		config.addIcon("DSAVIcon_128.png", FileType.Internal);
+		config.addIcon("DSAVIcon_32.png", FileType.Internal);
+		config.addIcon("DSAVIcon_16.png", FileType.Internal);
 		//config.resizable = false;
 		new LwjglApplication(new DSAVisualiser(), config);
 	}
